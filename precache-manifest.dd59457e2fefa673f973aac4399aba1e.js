0f2e49d6ac01711139d2f191237593fde86f368b
Lucas Souza <lucasosouza@gmail.com>
2019-01-08 23:10:12 -0200

    initial commit

---
+++ precache-manifest.dd59457e2fefa673f973aac4399aba1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1fc321d2b5aeab7c4f",
+    "url": "/static/css/main.ee60d5fd.chunk.css"
+  },
+  {
+    "revision": "ff1fc321d2b5aeab7c4f",
+    "url": "/static/js/main.ff1fc321.chunk.js"
+  },
+  {
+    "revision": "fffd4e55788b4f53e8f6",
+    "url": "/static/js/1.fffd4e55.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f1b2680d57d73fce147333ed73995013",
+    "url": "/index.html"
+  }
+];
